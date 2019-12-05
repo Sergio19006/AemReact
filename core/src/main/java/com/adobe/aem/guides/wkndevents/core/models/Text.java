da4a97e84f99b94a45b231d0a29367c0e765246f
Sergio <sergiodphn@gmail.com>
2019-12-05 11:06:12 +0000

    All is working Fine

--- core/src/main/java/com/adobe/aem/guides/wkndevents/core/models/Text.java
+++ core/src/main/java/com/adobe/aem/guides/wkndevents/core/models/Text.java
@@ -1,14 +1,41 @@
 package com.adobe.aem.guides.wkndevents.core.models;
 
-import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-@Model(adaptables=Resource.class)
-public class ModelA {
+import com.adobe.cq.export.json.ExporterConstants;
+import com.adobe.cq.export.json.ComponentExporter;
 
-    private String name;
+@Model(
+        adaptables = SlingHttpServletRequest.class,
+        adapters = {ComponentExporter.class},
+        resourceType = Text.RESOURCE_TYPE
+)
+@Exporter(
+        name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
+        extensions = ExporterConstants.SLING_MODEL_EXTENSION
+)
+public class Text implements ComponentExporter {
 
-    public String getName() {
-        return name;
+    private static final String PREPEND_MSG = "Que pasaaa locoooooooo";
+    static final String RESOURCE_TYPE = "wknd-events/components/content/text";
+
+    @ValueMapValue
+    @Optional
+    private String text;
+
+    public String getText() {
+        if (text != null && text.length() > 0) {
+            return PREPEND_MSG + " " + text;
+        }
+        return null;
+    }
+
+    @Override
+    public String getExportedType() {
+        return RESOURCE_TYPE;
     }
 }
